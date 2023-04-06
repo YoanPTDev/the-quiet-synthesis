060bb4717ec7003d97a0286c7a240aa856a9bd0e
YoanPTDev <yoanPTDev@outlook.com>
2023-04-06 12:31:58 -0400

    Working in progress pour les Cartes

---
+++ client/src/reducers/deck.js
@@ -0,0 +1,9 @@
+import { DECK, DECK_DRAW } from '../actions/types';
+import fetchStates from './fetchStates';
+
+const DEFAULT_DECK = {
+  deck_id: '',
+  fetchState: '',
+  message: '',
+  cards: []
+};
