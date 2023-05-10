a3d442c734d5a54c9279a26fd3ee6b831766959f
YoanPTDev <yoanPTDev@outlook.com>
2023-05-10 10:30:01 -0400

    dice value and card rewritten

--- client/src/components/AdventureLogInputWrapper.js
+++ client/src/components/AdventureLogInputWrapper.js
@@ -1,19 +1,53 @@
-import React, {useContext} from 'react';
+import React, { useContext, useState } from 'react';
 import { connect } from 'react-redux';
 import { SocketContext } from '../middleware/socketcontext';
 import { collapseAdventureLogInput } from '../actions/settings';
-import TextAreaField from "./TextAreaField";
-import { SAVE_ACTION_DATA } from "../../../utils/constants.mjs";
+import TextAreaField from './TextAreaField';
+import one from '../assets/one.png';
+import two from '../assets/two.png';
+import three from '../assets/three.png';
+import four from '../assets/four.png';
+import five from '../assets/five.png';
+import six from '../assets/six.png';
+import { SAVE_ACTION_DATA } from '../../../utils/constants.mjs';
+
+const diceNum = {
+  1: one,
+  2: two,
+  3: three,
+  4: four,
+  5: five,
+  6: six,
+};
+
+const DiceGadget = ({ onRoll }) => {
+  const [diceValue, setDiceValue] = useState(1);
+
+  const handleRoll = () => {
+    const newValue = (diceValue % 6) + 1;
+    setDiceValue(newValue);
+    onRoll(newValue);
+  };
+
+  return (
+    <img
+      src={diceNum[diceValue]}
+      alt={`Dice face ${diceValue}`}
+      onClick={handleRoll}
+    />
+  );
+};
 
 const AdventureLogInput = (props) => (
   <TextAreaField
     {...props}
-    placeholder="Add a description to your action..."
+    placeholder='Add a description to your action...'
     onSave={(value) => {
       if (value !== '') {
         const data = {
           type: 'DESCRIPTION_DATA',
           value: value,
+          turns: diceValue,
         };
         props.onSave(data);
       }
@@ -23,24 +57,28 @@ const AdventureLogInput = (props) => (
 );
 
 const AdventureLogInputWrapper = (props) => {
-  const { dispatch, adventureLogInputExpanded } =
-    props;
+  const { dispatch, adventureLogInputExpanded } = props;
 
-    if(!adventureLogInputExpanded) return null;
+  const socket = useContext(SocketContext);
 
-    const socket = useContext(SocketContext);
+  const handleDiceRoll = (value) => {
+    console.log(`Dice rolled: ${value}`);
+  };
 
-    return (
-      <div className='input-container'>
-        <AdventureLogInput 
+  if (!adventureLogInputExpanded) return null;
+
+  return (
+    <div className='input-container'>
+      <DiceGadget onRoll={handleDiceRoll} />
+      <AdventureLogInput
         onSave={(data) => {
           socket.emit(SAVE_ACTION_DATA, data);
         }}
         collapse={() => dispatch(collapseAdventureLogInput())}
-        />
-      </div>
-    )
-}
+      />
+    </div>
+  );
+};
 
 const mapStateToProps = (state) => {
   return {
